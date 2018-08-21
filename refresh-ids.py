49e913805d0dd86bcb2b9de50a467e2319092a27
l.maslovs <leonids@printify.com>
2018-08-21 15:18:39 +0300

    travis build

--- refresh-ids.py
+++ refresh-ids.py
@@ -11,16 +11,29 @@ def main():
     url = 'https://devternity-22e74.firebaseio.com/applications.json?auth=%s' % auth_token
 
     ticketsIds = []
+    errors = 0
     data = requests.get(url).json()
     for docid, doc in data.items():
+        if 'product' not in doc:
+            print("Document error (product): %s\n" % docid)
+            errors += 1
+            continue
+
         if doc['product'] != 'DT_RIX_18':
             continue
+
+        if 'tickets' not in doc:
+            print("Document error (tickets): %s\n" % docid)
+            errors += 1
+            continue
+
         for ticket in doc['tickets']:
             ticketsIds.append(ticket['id'])
 
     with open('./src/data/tickets.json', 'w') as w:
         w.write(json.dumps(ticketsIds, indent=True))
-
+    
+    print("Success: %d, Errors: %d" % (len(ticketsIds), errors))
 
 if __name__ == "__main__":
     main()
