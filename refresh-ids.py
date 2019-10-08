f64caacdc85eabc1b9c5d94969ad8671904352f2
l.maslovs <leonids@printify.com>
2019-10-08 18:16:48 +0300

    Revert "wip"
    This reverts commit 5209e46372d831de8146bc7c86516fbd02950a85.

--- refresh-ids.py
+++ refresh-ids.py
@@ -8,27 +8,27 @@ import json
 
 def main():
     auth_token = os.environ['AUTH_TOKEN']
-    url = 'https://devternity.firebaseio.com/registrations.json?auth=%s' % auth_token
+    url = 'https://devternity-22e74.firebaseio.com/applications.json?auth=%s' % auth_token
 
     ticketsIds = []
     errors = 0
     data = requests.get(url).json()
     for docid, doc in data.items():
-        # if 'product' not in doc:
-        #     print("Document error (product): %s\n" % docid)
-        #     errors += 1
-        #     continue
+        if 'product' not in doc:
+            print("Document error (product): %s\n" % docid)
+            errors += 1
+            continue
 
-        # if doc['product'] != 'DT_RIX_18':
-        #     continue
+        if doc['product'] != 'DT_RIX_18':
+            continue
 
-        # if 'tickets' not in doc:
-        #     print("Document error (tickets): %s\n" % docid)
-        #     errors += 1
-        #     continue
+        if 'tickets' not in doc:
+            print("Document error (tickets): %s\n" % docid)
+            errors += 1
+            continue
 
-        # for ticket in doc['tickets']:
-        ticketsIds.append(doc['ID'])
+        for ticket in doc['tickets']:
+            ticketsIds.append(ticket['id'])
 
     with open('./src/data/tickets.json', 'w') as w:
         w.write(json.dumps(ticketsIds, indent=True))
